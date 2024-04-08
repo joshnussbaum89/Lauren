6b4e95f2f7e1a050d5d33c467726a5e0713276b4
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2024-04-08 14:08:39 -0500

    add sitemap, remove unused image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
+import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://laurenvogelstein.com',
   build: {
     inlineStylesheets: 'always',
   },
-  integrations: [icon()],
+  integrations: [icon(), sitemap()],
 })

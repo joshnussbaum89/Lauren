6a89f8d294e0cd54ca781040002e25693f6b1f56
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-01-23 13:14:39 -0600

    feat(app): update deps, bg color, preload fonts, inlineStylesheets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,5 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://laurenvogelstein.com',
-  build: {
-    inlineStylesheets: 'always',
-  },
   integrations: [icon(), sitemap({ lastmod: new Date() })],
 })
